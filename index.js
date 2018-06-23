0b706d22f1cc3247579883b35587ae227a971a37
pablopawpaw <lpault29@gmail.com>
2018-06-23 18:46:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feetDistance) {
+  if(feetDistance > 2500) {
+    return `No can do.`;
+  } else if(feetDistance > 2000) {
+    return `I will gladly take your thirty bucks.`;
+  } else if(feetDistance <= 400) {
+    return `This one is on me!`;
+  }
+};
+
+function ternaryCheckCity(city) {
+  let result;
+  city === `NYC` ? result = `Ok, sounds good.` : result = `No go.`;
+  return result;
+};
+
+function switchOnCharmFromTip(word) {
+  let result;
+  switch(word) {
+    case `generous`:
+      result = `Thank you so much.`
+      break;
+    case `not as generous`:
+      result = `Thank you.`
+      break;
+    default:
+      result = `Bye.`;
+  };
+  return result
+};
